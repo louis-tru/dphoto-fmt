95ec295348f15c33305b1aa9a87cad8fa4fee9a6
chuxuewen <chuxuewen@hard-chain.cn>
2019-11-27 16:03:55 +0800

    up

--- center.js
+++ center.js
@@ -3,26 +3,58 @@
  * @date 2019-11-27
  */
 
+var keys = require('nxkit/keys');
+var fs = require('fs');
 var {FastMessageTransferCenter} = require('nxkit/fmt');
+const ADMIN_CACHE_TIME = 1e5; // 100s
 
 /**
  * @class Center
  */
 class Center extends FastMessageTransferCenter {
 
-	/**
-	 * @overwrite
-	 */
-	fnodeAuth(fnodeRemoteService) {
-		return true;
+	constructor(...args) {
+		super(...args);
+		this.m_admin = {};
+		this.m_adminTime = 0;
 	}
 
-	/**
-	 * @overwrite
-	 */
-	clientAuth(fmtService) {
+	_readAdminInfo(id) {
+		if (this.m_adminTime < Date.now()) {
+			var s;
+			if (fs.existsSync(__dirname + '/admin')) {
+				s = __dirname + '/admin';
+			} else if (fs.existsSync(__dirname + '/.admin')) {
+				s = __dirname + '/.admin';
+			}
+			if (s) {
+				this.m_admin = keys.parseFile(s);
+				this.m_adminTime = Date.now();
+			}
+		}
+		return this.m_admin[id];
+	}
+
+	authFnode(fnodeRemoteService) {
 		// TODO ...
-		return true;
+		return fnodeRemoteService.headers.certificate;
+	}
+
+	auth(fmtService) {
+		var {certificate,role} = fmtService.headers;
+		if (role == 'device') {
+			// TODO auth device ...
+			return certificate;
+		} else if (role == 'admin') {
+			var admin = this._readAdminInfo(fmtService.id);
+			if (admin) {
+				// TODO auth admin ...
+				if (admin == certificate) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
