a5e771d5d138a62954d9916fe513281a21e27fc6
chuxuewen <chuxuewen@hard-chain.cn>
2019-12-04 23:17:18 +0800

    up

--- log.js
+++ log.js
@@ -7,15 +7,13 @@ var utils = require('nxkit');
 var paths = require('./paths');
 var log = require('nxkit/log');
 
-if (!log.defaultConsole) {
-
-	if (!utils.dev || utils.options.caught_exception) {
-		process.on('uncaughtException', function(err) {
-			console.error(err);
-		});
-		process.on('unhandledRejection', (err, promise) => {
-			console.error(err);
-		});
-	}
-	new log.Console(paths.var + '/log.txt').makeDefault();
+if (!utils.dev || utils.options.caught_exception) {
+	process.on('uncaughtException', function(err) {
+		console.error(err);
+	});
+	process.on('unhandledRejection', (err, promise) => {
+		console.error(err);
+	});
 }
+
+new log.Console(paths.var + '/log.txt').makeDefault();
